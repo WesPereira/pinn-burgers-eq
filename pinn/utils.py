dff52acfc78d30a105ac36152d3e9108671fd546
WesPereira <wesleyalmeida@usp.br>
2021-12-08 22:39:25 -0300

    Fix: Change docstring

--- pinn/utils.py
+++ pinn/utils.py
@@ -3,9 +3,9 @@ import logging
 
 
 def get_log(log_name: str = 'root') -> logging.Logger:
-    '''
+    """
     Sets up logging configs
-    '''
+    """
     new_log = logging.getLogger(log_name)
     log_format = '%(asctime)-24s %(process)-2.2s %(threadName)-8.30s '\
                  '%(levelname)-8s %(name)10s:%(lineno)-4s- %(funcName)s: '\
